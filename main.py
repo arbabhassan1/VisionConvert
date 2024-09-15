ad9954ee4a7cb2c35fd24035056b080c894609d1
arbabhassan1 <linkto898@hotmail.com>
2024-09-15 15:32:50 +0500

    Complete Basic Funactionality

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 import os
+import cv2
 
-UPLOAD_FOLDER = 'static'
+UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = { 'webp', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.secret_key="C19A9252723DD986F6BF3995CB5B2"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
@@ -15,6 +17,28 @@ def allowed_file(filename):
 
 def processImage(filename,operation):
     print(f"the operation is {operation} and the file is {filename}")   
+    img=cv2.imread(f"uploads/{filename}")
+    match operation:
+        case "cgray":
+          imageProcessed=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+          cv2.imwrite(f"static/{filename}",imageProcessed)
+          return filename
+        case "cwebp":
+          newFileName=f"static/{filename.split('.')[0]}.webp"
+          cv2.imwrite(newFileName,img)
+          return newFileName
+        case "cjpg":
+          newFileName=f"static/{filename.split('.')[0]}.jpg"
+          cv2.imwrite(newFileName,img)
+          return newFileName
+       
+        case "cpng":
+          newFileName=f"static/{filename.split('.')[0]}.png"
+          cv2.imwrite(newFileName,img)
+          return newFileName
+        
+     
+          
     pass
 
 @app.route("/")
@@ -42,10 +66,13 @@ def edit():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            processImage(filename,operation)
+            new=  processImage(filename,operation)
+            flash(f"Your Image has been processed and is available here <a href='/{new}'>here</a>")
             return render_template("index.html")
     return render_template("index.html")
 
 
 
 app.run(debug=True, port=3000)
+
+# /elise-wilcox-VvuaRcjfvFQ-unsplash.png
